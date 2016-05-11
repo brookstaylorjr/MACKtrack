511ad60f6a455f937eac7a325250be33e71f08e8
brookstaylorjr <brookstaylorjr@gmail.com>
2016-05-11 12:03:39 -0700

    Added deets

--- CellScreen/screen_script.m
+++ CellScreen/screen_script.m
@@ -1,17 +1,18 @@
 % Single layout scans (define measurements, directory, and parameters, then call buildFromLayout)
 % Build measurment sets from layout files
 
-%% Prat's 4-plate experiment - April 8th, 2015
+%% Prat's 4-plate degradation/synthesis inhibition experiment - April 8th, 2015
 % Specify staring directory - we'll find ALL layouts in any subdirectories under this one
 start_dir = 'Z:\prat\20160428 Experiment';
 % Specify channels and measurements
 nuclear_channel = 'w1';
-measurement_channels = {'w2','w5'};
-measurement_type = {@mean,@median};
+measurement_channels = {'w2','w1'};
+measurement_type = {@mean,@mean};
 % Load parameters
-load('OP9_prat.mat')
+load('OP9_default.mat')
 % Find all plate layouts in parent directory; segment and measure cells
-[ScreenData, layout_dir, image_dir] = buildFromLayout(start_dir, nuclear_channel, measurement_channels, measurement_type, parameters)
+[ScreenData, layout_dir, image_dir] = ...
+    buildFromLayout(start_dir, nuclear_channel, measurement_channels, measurement_type, parameters);
 % Save output file(s)
 save([start_dir,filesep,'ScreenData_',date,'.mat'],'ScreenData')
 disp('... saved file')
