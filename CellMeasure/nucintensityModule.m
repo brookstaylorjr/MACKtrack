88b62b1ceb00ee2b2a1331137bc136ca104fab2c
Brooks Taylor <brooks@Brookss-MacBook-Pro.local>
2016-06-21 08:46:42 -0700

    Added optional 2nd channel measurement

--- CellMeasure/nucintensityModule.m
+++ CellMeasure/nucintensityModule.m
@@ -1,4 +1,4 @@
-function [CellMeasurements, ModuleData] = nucintensityModule(CellMeasurements,parameters, labels, AuxImages, ModuleData)
+function [CellMeasurements, ModuleData] = nucintensityModule(CellMeasurements, parameters, labels, AuxImages, ModuleData)
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 % INTENSITYMODULE measures cellular intensity in AuxImages{1} channel
 %
@@ -12,11 +12,11 @@ function [CellMeasurements, ModuleData] = nucintensityModule(CellMeasurements,pa
 
 iteration  = ModuleData.iter;
 % Mode-balance 1st auxililiary image - unimodal distribution assumed
-    if ~isfield(ModuleData,'distr')
-        [~, ModuleData.distr] = modebalance(AuxImages{1},1,ModuleData.BitDepth,'measure'); 
-    else
-        AuxImages{1} = modebalance(AuxImages{1},1,ModuleData.BitDepth,'correct',ModuleData.distr);
-    end
+if ~isfield(ModuleData,'distr')
+    [~, ModuleData.distr] = modebalance(AuxImages{1},1,ModuleData.BitDepth,'measure'); 
+else
+    AuxImages{1} = modebalance(AuxImages{1},1,ModuleData.BitDepth,'correct',ModuleData.distr);
+end
 
 
 % On first call, initialize all new CellMeasurements fields 
@@ -34,5 +34,39 @@ for n = 1:length(cells)
     CellMeasurements.IntegratedIntensityNuc(cells(n),iteration) = nansum(AuxImages{1}(labels.Nucleus==cells(n)));
     CellMeasurements.MedianIntensityNuc(cells(n),iteration) = nanmedian(AuxImages{1}(labels.Nucleus==cells(n)));
 end
+
+
+% Measure nuclei in 2nd auxiliary, if it is specified
+if ~isempty(AuxImages{2})
+    % Mode-balance
+    if ~isfield(ModuleData,'distr2')
+        [~, ModuleData.distr2] = modebalance(AuxImages{2},1,ModuleData.BitDepth,'measure'); 
+    else
+        AuxImages{2} = modebalance(AuxImages{2},1,ModuleData.BitDepth,'correct',ModuleData.distr2);
+    end
+
+
+    % On first call, initialize all new CellMeasurements fields 
+    if ~isfield(CellMeasurements,'MeanIntensityNuc2')
+        % Intensity-based measurement initialization
+        CellMeasurements.MeanIntensityNuc2 =  nan(parameters.TotalCells,parameters.TotalImages);
+        CellMeasurements.IntegratedIntensityNuc2 =  nan(parameters.TotalCells,parameters.TotalImages);
+        CellMeasurements.MedianIntensityNuc2 = nan(parameters.TotalCells,parameters.TotalImages);
+    end
+
+    % Cycle through each cell and assign measurements
+    cells = unique(labels.Nucleus(labels.Nucleus>0));
+    for n = 1:length(cells)
+        CellMeasurements.MeanIntensityNuc2(cells(n),iteration) = nanmean(AuxImages{2}(labels.Nucleus==cells(n)));
+        CellMeasurements.IntegratedIntensityNuc2(cells(n),iteration) = nansum(AuxImages{2}(labels.Nucleus==cells(n)));
+        CellMeasurements.MedianIntensityNuc2(cells(n),iteration) = nanmedian(AuxImages{2}(labels.Nucleus==cells(n)));
+    end
+
+end
+
+
+
+
+
 
 ModuleDataOut = ModuleData;
