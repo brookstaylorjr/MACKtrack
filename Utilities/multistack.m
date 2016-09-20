fd60d0322a986c9560958a68a7882831ac28c162
Brooks Taylor <brooks@sr16-e4c4684a65.stanford.edu>
2016-09-19 22:40:34 -0700

    Parameter update; bug fix for trackPrimary

--- Utilities/multistack.m
+++ Utilities/multistack.m
@@ -32,6 +32,9 @@ handles.text1 = uicontrol('Style','text','String','','BackgroundColor',[1 1 1]);
 % Create slider
 handles.slider1  = uicontrol('Style', 'slider','Max',zlength,'Min',1,'Value',1,'SliderStep',[1/(zlength-1) 1/(zlength-1)],'BackgroundColor',[.99 .99 1]);
 set(handles.slider1,'Callback',{@slider_Callback,handles});
+% Make "Reset zoom" button
+handles.reset  = uicontrol('Style', 'pushbutton','String','Reset zoom','BackgroundColor',[.99 .99 1]);
+set(handles.reset,'Callback',{@reset_Callback,handles});
 % Display graph
 set(handles.axes1,'YTick',[],'XTick',[])
 set(handles.figure1,'ResizeFcn',{@fig_resize,handles},'Toolbar','figure');
@@ -59,6 +62,19 @@ set(handles.axes1,'YTick',[],'XTick',[])
 
 % ========================================================================================
 
+function reset_Callback(~,~,handles)
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+% Reset zoom state of figure
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+xlim = [1 size(handles.input,2)];
+ylim = [1 size(handles.input,1)];
+
+set(gca,'xlim',xlim,'ylim',ylim)
+
+% ========================================================================================
+
+
 function fig_resize(~,~,handles)
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 % Resize figure and  slider smoothly
@@ -66,8 +82,9 @@ function fig_resize(~,~,handles)
 figPos = get(handles.figure1,'Position');
 h = figPos(4);
 w = figPos(3);
-set(handles.slider1,'Position',[floor(w/2)-200 5 400 20])
-set(handles.text1,'Position',[floor(w/2)+202,2, 80,20]);
+set(handles.slider1,'Position',[floor(w/2)-300 5 400 20])
+set(handles.reset,'Position',[floor(w/2)+182,8, 60, 18]);
+set(handles.text1,'Position',[floor(w/2)+102,5, 80, 20]);
 axis image
 set(handles.axes1,'OuterPosition',[0 20/h 1 (h-32)/h],'LooseInset',get(handles.axes1,'TightInset')+[10/w 10/h 10/w 10/h])
 set(handles.axes1,'YTick',[],'XTick',[])
