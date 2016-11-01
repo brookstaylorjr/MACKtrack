adc5f5dfe75aef3cc8507e07d2070ed2936214c0
Brooks Taylor <brooks@sr16-e4c4684a65.stanford.edu>
2016-11-01 15:32:46 -0700

    More normalization tests.

--- CellMeasure/screen_nuc_Module.m
+++ CellMeasure/screen_nuc_Module.m
@@ -18,8 +18,8 @@ measure_cc = label2cc(labels.Nucleus,0);
 
 % Mode-balance 1st auxililiary image - bimodal distribution assumed (nuclear expression, cytoplasmic expression, and b.g.)
 corr_img = AuxImages{1};
-corr_img(labels.Nucleus>0) = []; % Drop foreground objects for correction calculation
-[~, dist1] = modebalance(corr_img,2,ModuleData.BitDepth,'measure'); 
+corr_img(imdilate(labels.Nucleus>0,diskstrel(parameters.MinNucleusRadius))) = []; % Drop foreground objects for correction calculation
+[~, dist1] = modebalance(corr_img,1,ModuleData.BitDepth,'measure'); 
 AuxImages{1} = (AuxImages{1} - dist1(1))/dist1(2); % Background subtract/divide
 
 % Intensity-based measurement initialization
@@ -41,7 +41,7 @@ end
 if ~isempty(AuxImages{2})
     % Mode-balance 1st auxililiary image - bimodal distribution assumed (nuclear expression, cytoplasmic expression, and b.g.)
     corr_img = AuxImages{2};
-    corr_img(labels.Nucleus>0) = []; % Drop foreground objects for correction calculation
+    corr_img(imdilate(labels.Nucleus>0,diskstrel(parameters.MinNucleusRadius))) = []; % Drop foreground objects for correction calculation
     [~, dist1] = modebalance(corr_img,2,ModuleData.BitDepth,'measure');
     AuxImages{2} = (AuxImages{2} - dist1(1))/dist1(2); % Background subtract/divide
 
