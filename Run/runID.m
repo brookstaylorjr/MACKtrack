aa0168342a5eea47f6c2ba008d489f234b56a8b0
Brooks Taylor <brooks@Brookss-MacBook-Pro.local>
2016-11-12 13:59:58 -0800

    Addl functionality for runID.

--- Run/runID.m
+++ Run/runID.m
@@ -3,7 +3,8 @@ function [] = runID(varargin)
 % [] = runID(varargin)
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 % RUNID queues and tracks multiple experimental sets from the "Scope Runs" Google Doc -  
-% choose sets by ID number.
+% choose sets by ID number. Alternately, you can also pass a parameters structure, or file
+% location.
 %
 % INPUT:
 % varargin     ID# of sets to track (1st column of spreadsheet) - single values or vector
@@ -11,7 +12,7 @@ function [] = runID(varargin)
 % Example Usage: runID(1,4:16,27) will sequentially track sets 1, 4 to 16, and 27.
 %- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-% Specify save directory and load spreadsheet URL
+% Load scope/data locations
 home_folder = mfilename('fullpath');
 slash_idx = strfind(home_folder,filesep);
 home_folder = home_folder(1:slash_idx(end-1));
@@ -20,34 +21,78 @@ if ~exist(locations.scope)
     error(['Invalid mount location for images: "',locations.scope,...
         '" not found. Please load, update, and re-save "locations.mat"'])
 end
-[~,status] = urlread(locations.spreadsheet);
-if ~status
-    error(['Spreadsheet URL load unsuccessful. Please load, update, and re-save "locations.mat"'])
-end
-
-% Read in content from the "Scope Runs - Tracking Sets" spreadsheet
-data = readScopeRuns(locations.spreadsheet, cell2mat(varargin));
 
 
-% Display the sets we're running
-for idx = 1:numel(data.save_folder)
-    disp(['- ',data.save_folder{idx}])
-end
-
-
-% Cycle/measure sets
-for idx = 1:numel(data.save_folder)
+% OPTION 1: pass indicies corresponding to row entries in a spreadsheet.
+if isnumeric(varargin{1})
     
-    % PARAMETERS
-    load([home_folder,'Parameters',filesep,data.parameter_files{idx}])
-    parameters.ImagePath = data.image_paths{idx};
-    parameters.TimeRange = eval(data.time_ranges{idx});
-    parameters.XYRange = eval(data.xy_ranges{idx});
-    parameters.SaveDirectory = [data.save_dir{idx},filesep,data.save_folder{idx}];
-    clear p;
-    eval(data.modify{idx});
-    if exist('p','var'); parameters = combinestructures(p,parameters); end;
+    % Load scope runs spreadsheet
+    [~,status] = urlread(locations.spreadsheet);
+    if ~status
+        error(['Spreadsheet URL load unsuccessful. Please load, update, and re-save "locations.mat"'])
+    end
 
+    % Read in content from the "Scope Runs - Tracking Sets" spreadsheet
+    data = readScopeRuns(locations.spreadsheet, cell2mat(varargin));
+
+
+    % Display the sets we're running
+    for idx = 1:numel(data.save_folder)
+        disp(['- ',data.save_folder{idx}])
+    end
+
+
+    % Cycle/measure sets
+    for idx = 1:numel(data.save_folder)
+
+        % PARAMETERS
+        load([home_folder,'Parameters',filesep,data.parameter_files{idx}])
+        parameters.ImagePath = data.image_paths{idx};
+        parameters.TimeRange = eval(data.time_ranges{idx});
+        parameters.XYRange = eval(data.xy_ranges{idx});
+        parameters.SaveDirectory = [data.save_dir{idx},filesep,data.save_folder{idx}];
+        clear p;
+        eval(data.modify{idx});
+        if exist('p','var'); parameters = combinestructures(p,parameters); end;
+
+        mkdir([locations.data,filesep,parameters.SaveDirectory])
+        % TRACKING
+        parfor i = 1:length(parameters.XYRange)
+            xyPos = parameters.XYRange(i);
+            try
+             trackLoop(parameters,xyPos)
+            catch ME
+                disp(['Error in tracking position ', num2str(xyPos),':' , ME.message])
+                for err = 1:length(ME.stack)
+                    disp(['-> ', ME.stack(err).name,', line ', num2str(ME.stack(err).line)])
+                end
+                error('killing tracking')
+            end
+        end
+
+        % MEASUREMENT
+        disp(['Measuring ', parameters.SaveDirectory,'...'])
+        try
+            MACKmeasure(parameters);      
+        catch ME
+            disp(['Error in measurement:' , ME.message])
+            for err = 1:length(ME.stack)
+                disp(['-> ', ME.stack(err).name,', line ', num2str(ME.stack(err).line)])
+            end
+            error('killing measurement')
+        end
+    end
+% OPTION2: parameters file (or structure) was passed directly    
+else
+    if ischar(varargin{1})
+        load(varargin{1})
+    elseif isstruct(varargin{1})
+        parameters = varargin{1};
+    else
+        error(['runID accepts a paraeters structure, file location, or row entries',...
+            'of experiments (from scope spreadsheet). Type ''help runID'' for further detail'])
+    end
+    
     mkdir([locations.data,filesep,parameters.SaveDirectory])
     % TRACKING
     parfor i = 1:length(parameters.XYRange)
@@ -62,7 +107,7 @@ for idx = 1:numel(data.save_folder)
             error('killing tracking')
         end
     end
-    
+
     % MEASUREMENT
     disp(['Measuring ', parameters.SaveDirectory,'...'])
     try
@@ -75,3 +120,4 @@ for idx = 1:numel(data.save_folder)
         error('killing measurement')
     end
 end
+    
